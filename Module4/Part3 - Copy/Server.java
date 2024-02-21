54ec9c3abb4cd67aba4a3fb9a0ff0c0cea1f1a28
fj29 <125627007+fj29@users.noreply.github.com>
2024-02-21 05:40:20 +0500

    Update Server.java

--- Module4/Part3 - Copy/Server.java
+++ Module4/Part3 - Copy/Server.java
@@ -9,12 +9,15 @@ import java.util.List;
 
 public class Server {
     int port = 3001;
-    // connected clients
     private List<ServerThread> clients = new ArrayList<ServerThread>();
+    private boolean isGameActive = false;
+    private int hiddenNumber;
 
+
+    //UCID:FJ28
+    //DATE:2/21/2024
     private void start(int port) {
         this.port = port;
-        // server listening
         try (ServerSocket serverSocket = new ServerSocket(port);) {
             Socket incoming_client = null;
             System.out.println("Server is listening on port " + port);
@@ -23,11 +26,11 @@ public class Server {
                 if (incoming_client != null) {
                     System.out.println("Client connected");
                     ServerThread sClient = new ServerThread(incoming_client, this);
-                    
+
                     clients.add(sClient);
                     sClient.start();
                     incoming_client = null;
-                    
+
                 }
             } while ((incoming_client = serverSocket.accept()) != null);
         } catch (IOException e) {
@@ -37,24 +40,56 @@ public class Server {
             System.out.println("closing server socket");
         }
     }
-    protected synchronized void disconnect(ServerThread client) {
-		long id = client.getId();
-        client.disconnect();
-		broadcast("Disconnected", id);
-	}
-    
-    protected synchronized void broadcast(String message, long id) {
-        if(processCommand(message, id)){
 
+    protected synchronized void disconnect(ServerThread client) {
+        long id = client.getId();
+        client.disconnect();
+        broadcast("Disconnected", id);
+    }
+ 
+    //UCID:FJ28
+    //DATE:2/21/2024
+    private void startNumberGuesser() {
+        isGameActive = true;
+        hiddenNumber = (int) (Math.random() * 100);
+        broadcast("Number guesser game has started! Guess a number between 0 and 100.", -1);
+    }
+
+    private void stopNumberGuesser() {
+        isGameActive = false;
+        broadcast("Number guesser game has stopped. Guesses will be treated as regular messages.", -1);
+    }
+
+    private void processGuess(String message, long clientId) {
+        if (isGameActive && message.startsWith("/guess")) {
+            int guessedNumber = Integer.parseInt(message.split(" ")[1]);
+
+            if (guessedNumber == hiddenNumber) {
+                broadcast("Congratulations! User[" + clientId + "] guessed the correct number " + guessedNumber + "!",
+                        -1);
+                stopNumberGuesser();
+            } else {
+                broadcast("User[" + clientId + "] guessed " + guessedNumber + " but it was not correct.", -1);
+            }
+        }
+    }
+
+    //UCID:FJ28
+    //DATE:2/21/2024
+    private void processCoinToss(String message, long clientId) {
+        if (message.equalsIgnoreCase("/flip") || message.equalsIgnoreCase("/toss") || message.equalsIgnoreCase("/coin")) {
+            String result = (Math.random() < 0.5) ? "Heads" : "Tails";
+            broadcast("User[" + clientId + "] flipped a coin and got " + result + "!", -1);
+        }
+    }
+
+    protected synchronized void broadcast(String message, long id) {
+        if (processCommand(message, id)) {
             return;
         }
-        // let's temporarily use the thread id as the client identifier to
-        // show in all client's chat. This isn't good practice since it's subject to
-        // change as clients connect/disconnect
+
         message = String.format("User[%d]: %s", id, message);
-        // end temp identifier
-        
-        // loop over clients and send out the message
+
         Iterator<ServerThread> it = clients.iterator();
         while (it.hasNext()) {
             ServerThread client = it.next();
@@ -65,25 +100,37 @@ public class Server {
                 broadcast("Disconnected", id);
             }
         }
+
+        if (isGameActive) {
+            processGuess(message, id);
+        } else {
+            processCoinToss(message, id);
+        }
     }
 
-    private boolean processCommand(String message, long clientId){
+    private boolean processCommand(String message, long clientId) {
         System.out.println("Checking command: " + message);
-        if(message.equalsIgnoreCase("disconnect")){
+        if (message.equalsIgnoreCase("/disconnect")) {
             Iterator<ServerThread> it = clients.iterator();
             while (it.hasNext()) {
                 ServerThread client = it.next();
-                if(client.getId() == clientId){
+                if (client.getId() == clientId) {
                     it.remove();
                     disconnect(client);
-                    
                     break;
                 }
             }
             return true;
+        } else if (message.equalsIgnoreCase("/start")) {
+            startNumberGuesser();
+            return true;
+        } else if (message.equalsIgnoreCase("/stop")) {
+            stopNumberGuesser();
+            return true;
         }
         return false;
     }
+
     public static void main(String[] args) {
         System.out.println("Starting Server");
         Server server = new Server();
@@ -91,8 +138,6 @@ public class Server {
         try {
             port = Integer.parseInt(args[0]);
         } catch (Exception e) {
-            // can ignore, will either be index out of bounds or type mismatch
-            // will default to the defined value prior to the try/catch
         }
         server.start(port);
         System.out.println("Server Stopped");
